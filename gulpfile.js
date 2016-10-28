4d85eb683f00f68ebf0c6904cc62768c9d6ae8b9
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2016-10-29 00:52:31 +0200

    patch plugins and build to work with new speaker console

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,8 @@ gulp.task('js', ['clean:js'], function() {
     .on('error', browserifyPlumber)
     .pipe(source('src/scripts/main.js'))
     .pipe(buffer())
-    .pipe(isDist ? uglify() : through())
+    //uglyify doesn't currently work when using ES6 syntax (perhaps requires upgrade?)
+    //.pipe(isDist ? uglify() : through())
     .pipe(rename('build.js'))
     .pipe(gulp.dest('dist/build'))
     .pipe(connect.reload());
@@ -111,20 +112,20 @@ gulp.task('watch', function() {
 gulp.task('build-branch', ['build'], function() {
   var branch = git.branch();
   if (branch != 'master') {
-    return gulp.src('dist/**/*')
-      .pipe(gulp.dest('dist/@' + branch));
+    return gulp.src('dist/**/*').pipe(gulp.dest('dist/event/' + branch));
   }
 });
 
 gulp.task('publish', ['clean', 'build-branch'], function(done) {
   var branch = git.branch();
-  var opts = { logger: gutil.log }
+  var opts = { logger: gutil.log, message: 'Publish presentation from ' + branch + ' to GitHub pages' }
   if (branch != 'master') {
-    opts.src = (opts.only = '@' + branch) + '/**/*';
+    opts.src = (opts.only = 'event/' + branch) + '/**/*';
   }
   else {
     // only is a list of globby expressions (not git pathspecs)
-    opts.only = ['*', '!@*']
+    // NOTE don't set this option when first creating the gh-pages branch
+    opts.only = ['*', 'event']
   }
   ghpages.publish(path.join(__dirname, 'dist'), opts, done);
 });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,4 @@
 var bespoke = require('bespoke'),
-  bullets = require('bespoke-bullets'),
   classes = require('bespoke-classes'),
   extern = require('bespoke-extern'),
   hash = require('bespoke-hash'),
@@ -9,14 +8,149 @@ var bespoke = require('bespoke'),
   overview = require('bespoke-overview'),
   scale = require('bespoke-scale');
 
+function bullets(options) {
+  return function(deck) {
+    var activeSlideIndex,
+      activeBulletIndex,
+
+      bullets = deck.slides.map(function(slide) {
+        return [].slice.call(slide.querySelectorAll((typeof options === 'string' ? options : '[data-bespoke-bullet]')), 0);
+      }),
+
+      next = function() {
+        var nextSlideIndex = activeSlideIndex + 1;
+
+        if (activeSlideHasBulletByOffset(1)) {
+          activateBullet(activeSlideIndex, activeBulletIndex + 1);
+          return false;
+        } else if (bullets[nextSlideIndex]) {
+          activateBullet(nextSlideIndex, 0);
+        }
+      },
+
+      prev = function() {
+        var prevSlideIndex = activeSlideIndex - 1;
+
+        if (activeSlideHasBulletByOffset(-1)) {
+          activateBullet(activeSlideIndex, activeBulletIndex - 1);
+          return false;
+        } else if (bullets[prevSlideIndex]) {
+          activateBullet(prevSlideIndex, bullets[prevSlideIndex].length - 1);
+        }
+      },
+
+      activateBullet = function(slideIndex, bulletIndex) {
+        activeSlideIndex = slideIndex;
+        activeBulletIndex = bulletIndex;
+
+        bullets.forEach(function(slide, s) {
+          slide.forEach(function(bullet, b) {
+            bullet.classList.add('bespoke-bullet');
+
+            if (s < slideIndex || s === slideIndex && b <= bulletIndex) {
+              bullet.classList.add('bespoke-bullet-active');
+              bullet.classList.remove('bespoke-bullet-inactive');
+            } else {
+              bullet.classList.add('bespoke-bullet-inactive');
+              bullet.classList.remove('bespoke-bullet-active');
+            }
+
+            if (s === slideIndex && b === bulletIndex) {
+              bullet.classList.add('bespoke-bullet-current');
+            } else {
+              bullet.classList.remove('bespoke-bullet-current');
+            }
+          });
+        });
+      },
+
+      activeSlideHasBulletByOffset = function(offset) {
+        return bullets[activeSlideIndex][activeBulletIndex + offset] !== undefined;
+      };
+
+    deck.on('next', next);
+    deck.on('prev', prev);
+
+    deck.on('slide', function(e) {
+      activateBullet(e.index, 0);
+    });
+
+    activateBullet(0, 0);
+
+    // patch
+    deck.activateBullet = activateBullet
+    deck.slides.forEach((slide, slideIdx) => {
+      slide.bullets = bullets[slideIdx]
+    })
+  };
+}
+
 bespoke.from({ parent: 'article.deck', slides: 'section' }, [
   classes(),
   scale(),
   nav(),
   overview(),
   bullets('.build, .build-items > *:not(.build-items)'),
-  onstage(),
   hash(),
   multimedia(),
-  extern(bespoke)
+  function (deck) {
+
+    const metas = {}
+    Array.from(document.querySelectorAll('head meta')).forEach((meta) => {
+        metas[meta.getAttribute('name')] = meta.getAttribute('content')
+    })
+
+    const steps = deck.slides.map((slide, slideIdx) => {
+
+      const notes = [].slice.call(slide.querySelectorAll('aside[role="note"] p, aside[role="note"] li'))
+        .map((note) => note.textContent)
+        .join('\n')
+
+      if (slide.bullets.length > 0) {
+        return slide.bullets.map((b, bulletIdx) => {
+          return {
+            cursor: String(slideIdx) + '.' + String(bulletIdx),
+            states: [],
+            notes,
+          }
+        })
+      }
+
+      return {
+        cursor: String(slideIdx),
+        states: [],
+        notes,
+      }
+    })
+
+    const details = {
+      title: document.title || '',
+      authors: metas.author || '',
+      description: metas.description || '',
+      vendor: 'bespoke.js',
+      steps,
+      ratios: ['16/9'],
+      themes: ['default'],
+    }
+
+    window.addEventListener('message', ({ source, data: { command, commandArgs } }) => {
+
+      switch (command) {
+
+        case 'get-slide-deck-details':
+          source.postMessage({ event: 'slide-deck-details', eventData: { details } }, '*')
+          break;
+
+        case 'go-to-step':
+          const { cursor } = commandArgs
+          const [slideIdx, subslideIdx] = cursor.split('.')
+          deck.slide(Number(slideIdx))
+          deck.activateBullet(Number(slideIdx), Number(subslideIdx))
+          break;
+
+        default:
+          console.debug(`unknown protocol command ${command} with args`, commandArgs)
+      }
+    })
+  }
 ]);
